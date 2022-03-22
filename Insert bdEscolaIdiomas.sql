0939a76c0873c7ce9a32d838ad9aedef9b399260
DuarteGustavo77 <93735555+DuarteGustavo77@users.noreply.github.com>
2022-03-22 11:20:00 -0300

    Add files via upload

---
+++ Insert bdEscolaIdiomas.sql
@@ -0,0 +1,10 @@
+USE bdEscolaIdiomas
+
+INSERT INTO tbAluno(codAluno, nomeAluno, dataNascAluno, rgAluno, naturalidadeAluno)
+VALUES (1, 'Paulo Santos', 10/03/2000, '822821220', 'SP')
+, (2, 'Maria da Gloria', 03/10/1999, '452331230', 'SP')
+, (3, 'Pedro Nogueira da Silva', 04/04/1998, '235332119', 'SP')
+, (4, 'Gilson Barros Silva', 09/09/1995, '34221111x', 'PE')
+, (5, 'Mariana Barbosa Santos', 10/10/2001, '542221229', 'RJ')
+, (6, 'Alessandro Pereira', 11/10/2003, '244024549', 'ES')
+, (7, 'Aline Melo', 08/10/2001, '883650123', 'RJ')
